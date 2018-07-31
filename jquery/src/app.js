04cfad236a7f43295d085c82cec77ea8974139eb
akashbhatt <akashbhatt.in@gmail.com>
2018-07-31 16:36:53 +0530

    436

--- jquery/src/app.js
+++ jquery/src/app.js
@@ -1,6 +1,14 @@
 var ofs={};
+//debugger;
 ofs.add= function(){
-    var result = $('name');
-    console.log('test', result);
+    var result = $('#name');
+    console.log('test', result[0]);
 }
 ofs.add(1,true);
+document.body.appendChild(document.createElement('input'));
+var newelem = $('<input>');
+document.body.appendChild(newelem[0]);
+function search()
+{
+    console.log("testing..");
+}
